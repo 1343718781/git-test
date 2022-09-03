8cb057417d4e13a3e581098effec40d5edf057c4
1343718781 <97582915+1343718781@users.noreply.github.com>
2022-09-03 13:58:30 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Hello master!");
         System.out.println("Hello hot-fix!");
         System.out.println("Hello github!");
+        System.out.println("Hello github2!");
     }
 }
