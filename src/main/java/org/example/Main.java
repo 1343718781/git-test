5db5e2c8c2f5b5d5556e77a74c21f06f59e31f3c
wsx <1343718781@qq.com>
2022-09-03 13:57:51 +0800

    github commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Hello world4!");
         System.out.println("Hello master!");
         System.out.println("Hello hot-fix!");
+        System.out.println("Hello github!");
     }
 }
